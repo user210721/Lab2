fdaedfcffe6f3e1cd97f37010abe58fa4496bd37
EdwardTi <edwardti2004@gmail.com>
2023-10-31 04:01:59 +0800

    return values

--- bmi.py
+++ bmi.py
@@ -7,10 +7,13 @@ def calculate_bmi(height, weight):
 
     if bmi<18.5:
         print("User is Under Weight")
+        return -1
     elif 18.5<=bmi<=25.0:
         print("User is Normal Weight")
+        return 0
     else:
         print("User is Over Weight")
+        return 1
 
 
 calculate_bmi(weight=57, height=1.73)
